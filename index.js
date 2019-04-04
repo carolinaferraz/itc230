163e259241753c372fb3b57bd4dc464999de75d4
carolinaferraz <43863678+carolinaferraz@users.noreply.github.com>
2019-04-04 01:22:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const http = require("http"); 
+const fs = require("fs");
+
 http.createServer((req,res) => {
   const path = req.url.toLowerCase();
   switch(path) {
     case '/':
-    const fs = require("fs");
-    fs.readFile("home.html", (err, data) => {
+    fs.readFile("public/home.html", (err, data) => {
       if (err) return console.error(err);
       res.end(data.toString());
    });
       break;
+
     case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('hello, this is an application under construction, i still need to decide what i want it to do!');
-      break;
+    fs.readFile("public/about.html", (err, data) => {
+      if (err) return console.error(err);
+      res.end(data.toString());
+      });
+      break; 
+    
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('404 -- Not found :(');
